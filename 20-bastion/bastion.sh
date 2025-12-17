a9ed3741219182a670d6e61a1f677d4871cdbab8
Your Name <you@example.com>
2025-12-17 21:44:28 +0530

    msg

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -40,3 +40,5 @@ sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+
