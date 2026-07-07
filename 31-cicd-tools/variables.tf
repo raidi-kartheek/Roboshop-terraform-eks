fa4305182b43367cf46da6d223d111413bfbfe4f
Your Name <you@example.com>
2026-07-07 10:26:07 +0530

    msg

---
+++ 31-cicd-tools/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z09Z04182971BUBWKCCJ0S5U"
+}
+
+variable "domain_name" {
+    default = "raidi.shop"
+}
+
+variable "sonar" {
+    default = true
+}
+
+variable "jenkins" {
+    default = true
+}
+
+variable "runner" {
+    default = false
+}
