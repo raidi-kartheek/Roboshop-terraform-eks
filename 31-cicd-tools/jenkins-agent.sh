81bcd01b129b447c9f7c9a9403fb11df2d965d52
Your Name <you@example.com>
2026-07-08 12:56:22 +0530

    msg

--- 31-cicd-tools/jenkins-agent.sh
+++ 31-cicd-tools/jenkins-agent.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-growpart /dev/nvme0n1 4
-lvextend -L +10G /dev/mapper/RootVG-rootVol
-lvextend -L +10G /dev/mapper/RootVG-homeVol
-lvextend -L +10G /dev/mapper/RootVG-varVol
+sudo growpart /dev/xvda 4
+sudo lvextend -L +10G /dev/mapper/RootVG-rootVol
+sudo lvextend -L +10G /dev/mapper/RootVG-homeVol
+sudo lvextend -L +10G /dev/mapper/RootVG-varVol
 
-xfs_growfs /var
-xfs_growfs /home
-xfs_growfs /
+sudo xfs_growfs /var
+sudo xfs_growfs /home
+sudo xfs_growfs /
 
 # Java
 yum install fontconfig java-21-openjdk -y
